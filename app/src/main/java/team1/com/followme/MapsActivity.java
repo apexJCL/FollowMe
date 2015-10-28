e58068eda8dac9877de8f5fd1d01efd4e66fc2d1
José Carlos López <zero.chuck@gmail.com>
2015-10-27 18:30:46 -0600

    Maps Created, Debug API Key Generated. MainActivity added. (Keep Nav Pane?)

--- app/src/main/java/team1/com/followme/MapsActivity.java
+++ app/src/main/java/team1/com/followme/MapsActivity.java
@@ -37,10 +37,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng celaya = new LatLng(20.52, -100.81);
+        mMap.addMarker(new MarkerOptions().position(celaya).title("Celaya, Guanajuato"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(celaya));
     }
 }
